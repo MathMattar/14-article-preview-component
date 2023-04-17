7d78dbef3197cf811531823ad25dade5af8e6dca
MathMattar <mathmattar@hotmail.com>
2023-04-17 18:56:01 -0300

    Adaptations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Content from "./components/Content";
+import Background from "./components/Background";
+import Banner from "./components/Banner";
+import Card from "./components/Card";
+import Title from "./components/Title";
+import React from "react";
+import Profile from "./components/Profile";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Background>
+        <Banner />
+        <Card>
+          <Title>
+            <h1>
+              Shift the overall look and feel by adding these wonderful touches
+              to furniture in your home
+            </h1>
+          </Title>
+          <Content>
+            <p>
+              Ever been in a room and felt like something was missing? Perhaps
+              it felt slightly bare and uninviting. I’ve got some simple tips to
+              help you make any room feel complete.
+            </p>
+          </Content>
+          <Profile name="Michelle Appleton" date="28 Jun 2020" />
+        </Card>
+      </Background>
+    </>
   );
 }
-
-export default App;
